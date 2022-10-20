5b4300b68f512936c2b118feab1726e85c4d0300
imcodingdreams <naykavallejo@yahoo.com>
2022-10-20 16:04:43 -0700

    Add mathOperation display to script.js and fix numBtns and operatorBtns functions

--- script.js
+++ script.js
@@ -26,16 +26,17 @@ let decimalDotPresent = false;
 
 numberBtns.forEach(function(button) {
   button.addEventListener("click", (e) => {
-    let firstOperand = e.target.value;
-    if (e.target.innerText === "." && !decimalDotPresent) {
+    let firstOperand = e.target.innerText;
+    if (firstOperand === "." && !decimalDotPresent) {
     decimalDotPresent = true;
-    } else if (e.target.innerText === "." && decimalDotPresent) {
+    } else if (firstOperand === "." && decimalDotPresent) {
       return;
     } else if (display.innerText === "0") {
     display.innerText = "";
     decimalDotPresent = false;
     }
     firstOperand = e.target.innerText;
+    mathOperationDisplay.innerText += firstOperand;
     display.innerText += firstOperand;
     console.log(button.value); 
   });
@@ -43,7 +44,7 @@ numberBtns.forEach(function(button) {
 
 operatorBtns.forEach(function(button) {
   button.addEventListener("click", (e) => {
-    let operator = e.target.value;
+    let operator = e.target.innerText;
     decimalDotPresent = false;
     if (display.innerText === "0") {
       return;
@@ -53,9 +54,9 @@ operatorBtns.forEach(function(button) {
     display.textContent.includes("+")) {
     return;
     } 
-    // let buttonValue = operator.textContent;
-    // let displayValue = display.textContent;
-    display.textContent += operator;
+    //mathOperationDisplay.innerText += operator;
+    display.innerText = firstOperand;
+    mathOperationDisplay.innerText += operator;
     console.log(button.value);
   });
 });
@@ -63,6 +64,10 @@ operatorBtns.forEach(function(button) {
 ceBtn.forEach(function(button) {
   button.addEventListener("click", () => {
     display.innerText = '0';
+    mathOperationDisplay.innerText = "";
+    //firstOperand.innerText = "";
+    //operator.innerText = "";
+    //secondOperand.innerText = "";
     console.log(button.value);
   });
 });
@@ -74,16 +79,8 @@ memoryBtns.forEach(function(button) {
 });
 
 equalsBtn.forEach(function(button) {
-  button.addEventListener("click", (e) => {
-    let firstOperand = e.target;
-    let buttonValue = firstOperand.value;
-    let displayValue = display.textContent;
-
-    if (!displayValue === '0')
-    displayValue = '';
-    display.textContent = displayValue + buttonValue;
-    console.log(button.value); 
-    return;
+  button.addEventListener("click", () => {
+  
   });
 });
 
@@ -102,94 +99,3 @@ function calcMathOperation(firstOperand, secondOperand, operator) {
   }   
   return secondOperand;
 };
-
-
-
-
-
-
-
-
-// function calcMathOperation() {
-//   if (operator === "+") {
-
-//   }
-
-// }
-
-
-// let result = () => {
-
-// }
-
-
-
-
-
-
-
-
-
-// numberBtns.forEach(button => {
-//   button.addEventListener('click', function(e) {
-//     let clickedBtn = e.target;
-//     if (clickedBtn.classList.contains('number-btn')) {;
-//       console.log("You clicked " + clickedBtn.value);
-//     }
-//     if (clickedBtn.classList.contains('operator-btn')) {;
-//       console.log("You clicked " + clickedBtn.value);
-//     }
-//     if (clickedBtn.classList.contains('decimal-btn')) {;
-//       console.log("You clicked " + clickedBtn.value);
-//     }
-//     if (clickedBtn.classList.contains('equals-btn')) {;
-//       console.log("You clicked " + clickedBtn.value);
-//     }
-//     if (clickedBtn.classList.contains('ce-btn')) {;
-//       console.log("You clicked ce-btn " + clickedBtn.value);
-//     }
-//     if (clickedBtn.classList.contains('memory-btn')) {;
-//       console.log("You clicked memory-btn" + clickedBtn.value);
-//     }
-//     // intDisplayContainer.innerText = clickedBtn.value;
-//     // mathOperationDisplay.innerText = clickedBtn.value;
-//   })
-// });
-
-
-
-
-
-
-// let displaySecondOperand = document.getElementById('secondOperand');
-// document.querySelectorAll('.number-btn').forEach(item => {
-//   item.addEventListener('click', e => {
-//     let secondOperand = e.target;
-//     let calcDisplaySecondOperand = document.getElementById("calcDisplayFirstOperand");
-//     calcDisplaySecondOperand.innerText = firstOperand.value;
-//     displaySecondOperand.innerText = firstOperand.value;
-//   })
-// });
-
-
-// let calculatorDisplay = document.getElementById('intDisplayContainer');
-// let calculatorDisplayTop = document.getElementById('mathOperationDisplay');
-// let memoryDisplay = false;
-
-
-// function displayMathOperation(e) {
-//   const operator = e.target;
-//   if (operator === divOperator) {
-//     console.log("You clicked ÷")
-//     } else if 
-//     (operator === multOperator) {
-//     console.log("You clicked x")
-//     } else if
-//     (operator === subtOperator) {
-//     console.log("You clicked -")
-//     } else if
-//     (operator === sumOperator) {
-//     console.log("You clicked +")
-//   }   
-// };
-
